ddccf82b940f0937a2aa2a1b7a492edcd15d91fd
Guilherme David <guilhermedavidrk@gmail.com>
2023-02-09 20:06:51 -0300

    :wrench: config: prepare environment

--- App.tsx
+++ App.tsx
@@ -1,5 +1,33 @@
-import { View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
-  return <View></View>
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  return (
+    <View
+      style={{
+        justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#202024',
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? (
+        <Text style={{ color: '#fff', fontSize: 40 }}>Hello wolrd</Text>
+      ) : (
+        ''
+      )}
+    </View>
+  )
 }
